a011f1e88be7762ff8ec4ec7563a40babac76a89
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-07-26 23:54:43 -0500

    Types - numbers

---
+++ bases/types/numbers.ts
@@ -0,0 +1,17 @@
+(()=>{
+    let avengers: number = 10;
+    let villains: number = 20;
+
+    avengers = 30;
+
+    avengers = Number('15');
+
+    // NaN type is number, so typescript won't show an error when asigning NaN to a number type variable
+    // avengers = Number('124QA'); // result Nan
+
+    if(avengers < villains){
+        console.log('We are doomed');
+    } else {
+        console.log('Victory!');
+    }
+})();
