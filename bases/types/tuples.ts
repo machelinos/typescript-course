741636028a81526f8b0d9a26dffb7595bc3a7cd5
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-07-27 01:06:18 -0500

    Types - tuples

---
+++ bases/types/tuples.ts
@@ -0,0 +1,11 @@
+(()=>{
+    const hero: [string, number] = ['Dr. Strange', 42];
+
+    // with tuples we are specifying the type in each of the arrays indexes
+    // we cannot change the value at index to other type than specified
+    // hero[0] = 123; // => error Type 'number' is not assignable to type 'string'
+
+    console.log({ hero });
+
+
+})();
