8ffca96443ebb13fa38cd21cd61571a836990213
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-07-27 00:29:58 -0500

    Types - strings

---
+++ bases/types/strings.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    var _a;
+    const batman = 'Batman';
+    const greenLantern = 'Green Lantern';
+    console.log("I'm " + batman);
+    console.log(batman.toUpperCase());
+    console.log((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase());
+})();
---
+++ bases/types/strings.ts
@@ -0,0 +1,8 @@
+(()=>{
+    const batman: string = 'Batman';
+    const greenLantern: string = 'Green Lantern';
+    console.log("I'm "+ batman);
+    console.log(batman.toUpperCase());
+
+    console.log(batman[10]?.toUpperCase());
+})();
