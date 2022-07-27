2d492a7c5369830f002ff4345f3d850e93685617
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-07-27 00:59:57 -0500

    Types - arrays

---
+++ bases/types/arrays.ts
@@ -0,0 +1,13 @@
+(()=>{
+    const numbers: number[] = [1,2,3,4,5,6];
+
+    // You just can add elements of type number into the numbers array, another type will de marked as error in ts
+    // numbers.push('Wjatever') // => error
+
+    // if we loop through the array, intellisense will recognize the type of each array's element
+    numbers.forEach(number => {
+        console.log(number.toString());
+    })
+
+    console.log(numbers);
+})();
