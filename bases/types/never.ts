de949eaca8b604c067dc188d92e9d29bd2bb0fd0
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-07-27 01:28:24 -0500

    Types - never

---
+++ bases/types/never.ts
@@ -0,0 +1,11 @@
+(()=>{
+    const errorFunc = ( message: string ): never => {
+        // functions that give output of never must break at an error 
+        throw new Error(message);
+    }
+
+    errorFunc('This went sideways');
+
+    // This code is not going to execute
+    console.log('Hi!');
+})();
