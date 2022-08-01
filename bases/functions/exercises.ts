b6f477aaa74ef40468c8ec76ecda30e579007a3e
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-08-01 11:22:15 -0500

    Functions - exercises

---
+++ bases/functions/exercises.ts
@@ -0,0 +1,38 @@
+(()=>{
+
+// Funciones Básicas
+function sumar( a:number, b: number ): number{
+    return a + b;
+  }
+  
+  const contar = ( heroes: string[] ): number => {
+    return heroes.length;
+  }
+  const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+  contar(superHeroes);
+  
+  //Parametros por defecto
+  const llamarBatman = ( llamar: boolean = true ): void => {
+    if( llamar ){
+      console.log("Batiseñal activada");
+    }
+  }
+  
+  llamarBatman();
+  
+  // Rest?
+  const unirheroes = ( ...personas: string[] ): string => {
+    return personas.join(", ");
+  }
+  
+  console.log(unirheroes('Flash', 'Superman'))
+  
+  // Tipo funcion
+  const noHaceNada = ( numero: number, texto: string, booleano: boolean, arreglo: [] ): void=> {}
+  
+  // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+  let noHaceNadaTampoco: (numero: number, texto: string, booleano: boolean, arreglo: [])=>void;
+  noHaceNadaTampoco = noHaceNada
+  
+
+})();
