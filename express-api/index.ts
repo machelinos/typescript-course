4bff8e184863c446cc4a45525ee80c54613b8f7d
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-08-05 01:46:45 -0500

    Typescript in packages that dont use typescript

---
+++ express-api/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(401).json({
+    ok: false,
+    msg: 'Not authorized token'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
